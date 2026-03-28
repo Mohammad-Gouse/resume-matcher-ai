22a26c237e198cc8abefdd88e2fd5f39468d073d
Gouse Shaikh <Gouse.Shaikh@HAP-349s-MacBook-Air.local>
2026-03-28 22:45:52 +0530

    create job and match resume has been added

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Job(models.Model):
+    """
+    Job posted by recruiter
+    """
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="jobs",
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    required_skills = models.JSONField(default=list)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
